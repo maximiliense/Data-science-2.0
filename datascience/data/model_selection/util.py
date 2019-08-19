1fa182c0e74bf0041a0e6d66bb16f1dac8a175a7
mathilde <mathilde.negri@agroparistech.fr>
2019-08-19 16:41:41 +0200

    update

--- datascience/data/model_selection/util.py
+++ datascience/data/model_selection/util.py
@@ -14,7 +14,6 @@ def perform_split(dataset, test_size, splitter, random_state=42, **kwargs):
     quad_size = kwargs['quad_size']
     if len(dataset) == 0:
         print_errors('[perform_split] data should be a list, a tuple, etc. of positive size', do_exit=True)
-    print(quad_size)
     if 0 < test_size < 1 and len(dataset[0]) > 0:
         split_test = splitter(*dataset, test_size=test_size, random_state=random_state, quad_size=quad_size)
         train, test = split_test[::2], split_test[1::2]
