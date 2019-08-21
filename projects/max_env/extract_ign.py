cb28cca6812eb131fa4b94eb6d2ff9f7692eb1f7
Servajean Maximilien <maximiliense@gmail.com>
2019-08-21 13:52:15 +0200

    extract_ign

--- projects/max_env/extract_ign.py
+++ projects/max_env/extract_ign.py
@@ -4,15 +4,20 @@ from datascience.tools.ign.extract_patch import extract_patch
 from engine.parameters.special_parameters import get_parameters
 
 
+if get_parameters('source50cm', False):
+    source = 'ign_50cm_maps_and_patches'
+else:
+    source = 'ign_5m_maps_and_patches'
+
 if get_parameters('check_only', False):
-    check_extraction(source='full_ign')
+    check_extraction(source=source)
 else:
     if get_parameters('uncompress', True):
         # uncompress the IGN maps
-        extract_7z(source='full_ign')
+        extract_7z(source=source)
 
     # extract patches from a dataset and the IGN maps
-    extract_patch('full_ign', offset=get_parameters('offset', 0))
+    extract_patch(source, offset=get_parameters('offset', 0))
 
     # check extraction, save errors and filter dataset
-    check_extraction(source='full_ign')
+    check_extraction(source=source)
