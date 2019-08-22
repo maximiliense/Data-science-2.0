900f9eb09c12426536c2edad8145541ceaa956f8
zhelloz <benjamindeneu@free.fr>
2019-08-22 16:16:25 +0200

    update and bug fix, work in progress

--- datascience/ml/xgboost_skl/train.py
+++ datascience/ml/xgboost_skl/train.py
@@ -1,63 +1,67 @@
 import xgboost as xgb
 import numpy as np
-import ast
-
 from datascience.ml.evaluation import validate, export_results
+from datascience.ml.xgboost_skl.util import save_model, load_model
 from engine.parameters import special_parameters
-from engine.flags import incorrect_structure
-from engine.logging import print_logs, print_h1, print_notif
+from engine.logging import print_logs, print_h1, print_notif, print_errors
 from engine.core import module
-from engine.path import output_path
+from engine.logging.verbosity import debug, verbose
+from engine.hardware import use_gpu, first_device
+from engine.parameters.special_parameters import validation_only
 
-# TODO : debbug and improve
 
 @module
-def fit(model, train, test, validation_only=False, export=False, training_params=None, export_params=None):
+def fit(model, train, test, num_boost_round=360, verbose_eval=1, export=False, training_params=None, export_params=None, **kwargs):
+    if not use_gpu():
+        print_errors('XGBoost can only be executed on a GPU for the moment', do_exit=True)
+
     training_params = {} if training_params is None else training_params
     export_params = {} if export_params is None else export_params
 
-    dtest = xgb.DMatrix(np.asarray(test.get_vectors()), label=np.asarray(test.labels))
-    X_test = np.asarray(test.get_vectors())
-    y_test = np.asarray(test.labels)
+    d_test = xgb.DMatrix(np.asarray(test.get_vectors()), label=np.asarray(test.labels))
 
     if not validation_only:
         print_h1('Training: ' + special_parameters.setup_name)
         print_logs("get vectors...")
 
-        X_train = np.asarray(train.get_vectors())
-        y_train = np.asarray(train.labels)
+        X = np.asarray(train.get_vectors())
+        y = np.asarray(train.labels)
 
-        eval_set = [(X_test, y_test)]
+        d_train = xgb.DMatrix(X, label=y)
+
+        if debug:
+            verbosity = 3
+        elif verbose:
+            verbosity = 2
+        else:
+            verbosity = 0
+
+        gpu_id = first_device().index
+
+        kwargs['verbosity'] = verbosity
+        kwargs['gpu_id'] = gpu_id
+
+        eval_list = [(d_test, 'eval'), (d_train, 'train')]
 
         print_logs("fit model...")
 
-        model.fit(
-            X_train,
-            y_train,
-            eval_metric='merror',
-            early_stopping_rounds=20,
-            eval_set=eval_set
+        bst = xgb.train(
+            kwargs,
+            d_train,
+            num_boost_round=num_boost_round,
+            verbose_eval=verbose_eval,
+            evals=eval_list,
+            xgb_model=model
         )
 
         print_logs("Save model...")
-        complement = {'best_iteration': bst.best_ntree_limit}
-        with open(output_path("model_complement.txt"), "w") as file:
-            file.write(str(complement))
-        bst.save_model(output_path("model"))
-        bst.dump_model(output_path("model_dump"))
+        save_model(bst)
 
     else:
-        print_logs("load model " + special_parameters.output_path("_model"))
-        bst = xgb.Booster()
-        bst.load_model(output_path("model"))
-        with open(output_path("model_complement.txt"), "r") as file:
-            st = file.read()
-            complement = ast.literal_eval(st)
-        if 'best_iteration' in complement:
-            bst.best_ntree_limit = complement['best_iteration']
+        bst = load_model()
 
     print_h1('Validation/Export: ' + special_parameters.setup_name)
-    predictions = bst.predict(dtest, ntree_limit=bst.best_ntree_limit)
+    predictions = bst.predict(d_test, ntree_limit=bst.best_ntree_limit)
     res = validate(
         predictions, np.array(test.labels), training_params['metrics'] if 'metrics' in training_params else tuple(),
         final=True
--- datascience/ml/xgboost_skl/util.py
+++ datascience/ml/xgboost_skl/util.py
@@ -3,43 +3,48 @@ from engine.hardware import use_gpu, first_device
 from engine.parameters.special_parameters import from_scratch, nb_workers
 from engine.path.path import output_path
 from engine.logging.verbosity import debug, verbose
-from engine.logging.logs import print_debug, print_errors
+from engine.logging.logs import print_debug, print_errors, print_logs
 from sklearn.externals import joblib
-from xgboost import XGBClassifier
+from xgboost import Booster
+import ast
 
 
 @module
-def load_or_create(*args, **kwargs):
-    if not use_gpu():
-        print_errors('XGBoost can only be executed on a GPU for the moment', do_exit=True)
+def load_or_create(objective='multi:softprob', max_depth=2, seed=4242, eval_metric='merror',
+                   num_class=4520, num_feature=256, **kwargs):
+
     if from_scratch:
-        print_debug('Creating Random Forest Classifier')
-        if debug:
-            verbosity = 3
-        elif verbose:
-            verbosity = 2
-        else:
-            verbosity = 0
-        gpu_id = first_device().index
+        print_debug('Creating XGB Boosted Tree')
         params = {'updater': 'grow_gpu', 'predictor': 'gpu_predictor', 'tree_method': 'gpu_hist',
-                  'objective': 'multi:softprob', 'gpu_id': gpu_id}
-        model = XGBClassifier(
-            *args, **kwargs, verbosity=verbosity, n_jobs=nb_workers, seed=4242
+                  'eval_metric': eval_metric, 'objective': objective, 'num_class': num_class,
+                  'max_depth': max_depth, 'seed': seed, 'num_feature': num_feature}
+
+        params = {**params, **kwargs}
+
+        model = Booster(
+            params,
         )
-        model.set_params(**params)
     else:
         model = load_model()
 
     return model
 
 
-def load_model(ext=''):
-    path = output_path('models', ext + '_model.skl')
-    print_debug('Loading SKL model ' + path)
-    return joblib.load(path)
+def load_model():
+    print_logs("load model " + output_path("_model"))
+    bst = Booster()
+    bst.load_model(output_path("model"))
+    with open(output_path("model_complement.txt"), "r") as file:
+        st = file.read()
+        complement = ast.literal_eval(st)
+    if 'best_iteration' in complement:
+        bst.best_ntree_limit = complement['best_iteration']
+    return bst
 
 
 def save_model(model):
-    path = output_path('models', '_model.skl')
-    print_debug('Saving SKL model ' + path)
-    return joblib.dump(model, path)
+    complement = {'best_iteration': model.best_ntree_limit}
+    with open(output_path("model_complement.txt"), "w") as file:
+        file.write(str(complement))
+    model.save_model(output_path("model"))
+    model.dump_model(output_path("model_dump"))
