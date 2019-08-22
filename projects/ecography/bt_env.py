5213c0da88a4966a28e5ca7872d2161ad9c42421
zhelloz <benjamindeneu@free.fr>
2019-08-22 16:20:45 +0200

    update

--- projects/ecography/bt_env.py
+++ projects/ecography/bt_env.py
@@ -3,7 +3,6 @@ from datascience.ml.metrics.metrics import ValidationAccuracyRangeBySpecies, Val
 from datascience.data.loader import occurrence_loader
 from datascience.data.datasets import EnvironmentalDataset
 from datascience.ml.xgboost.train import fit
-from engine.parameters.special_parameters import validation_only
 from datascience.model_selection import train_test_split_stratified
 
 # loading dataset
@@ -17,4 +16,7 @@ training_params = {
                 ValidationAccuracyRangeBySpecies(max_top_k=100, final_validation=True),
                 ValidationAccuracyForAllSpecies(train=train, final_validation=True))
 }
-fit(train=train, test=test, training_params=training_params, validation_only=validation_only)
+fit(train=train, test=test, training_params=training_params,
+    objective='multi:softprob', max_depth=2, seed=4242, eval_metric='merror', num_class=4520,
+    num_boost_round=360, early_stopping_rounds=10, verbose_eval=1,updater='grow_gpu',
+    predictor='gpu_predictor', tree_method='gpu_hist')
