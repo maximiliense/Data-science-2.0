af8f3ff259bbef224094e2be3ec80c2baa9ecdf9
Servajean Maximilien <maximiliense@gmail.com>
2019-08-22 17:20:56 +0200

    util corrected

--- datascience/ml/sklearn/util.py
+++ datascience/ml/sklearn/util.py
@@ -1,32 +1,33 @@
 from engine.core import module
 from engine.parameters.special_parameters import from_scratch, nb_workers
 from engine.path.path import output_path
-from engine.logging.verbosity import debug, verbose
+from engine.logging.verbosity import is_debug, is_verbose
 from engine.logging.logs import print_debug
 from sklearn.externals import joblib
 
 
 @module
-def load_or_create(model_class, *args, **kwargs):
+def load_or_create(model_class, model_name='model', *args, **kwargs):
     if from_scratch:
-        print_debug('Creating Sklearn Model')
-        verbosity = 2 if debug and verbose else 0
+        print_debug('Creating SKL Model')
+
         model = model_class(
-            *args, **kwargs, verbose=verbosity, n_jobs=nb_workers
+            *args, **kwargs, verbose=2 if is_debug() and is_verbose() else 0, n_jobs=nb_workers
         )
+
     else:
-        model = load_model()
+        model = load_model(model_name=model_name)
 
     return model
 
 
 def load_model(model_name='model'):
     path = output_path('{}.skl'.format(model_name))
-    print_debug('Loading SKL model ' + path)
+    print_debug('Loading SKL model: ' + path)
     return joblib.load(path)
 
 
 def save_model(model, model_name='model'):
     path = output_path('{}.skl'.format(model_name))
-    print_debug('Saving SKL model ' + path)
+    print_debug('Saving SKL model: ' + path)
     return joblib.dump(model, path)
