3ee872366a375a64a5844d217fb68341daa6b21b
Servajean Maximilien <maximiliense@gmail.com>
2019-09-01 11:06:56 +0200

    autorestart Jean Zay

--- scripts/submit_scripts/jz.sh
+++ scripts/submit_scripts/jz.sh
@@ -8,50 +8,44 @@ walltime="00:10:00";
 
 script_dir=".";
 out="$HOME";
-
 usage(){
     indentation="                          ";
-	echo "Usage: submit python_file [-h] [-g NB_GPUS] [-c NB_CORES] [-d ROOT_DIR]";
-	echo "$indentation[-w WALLTIME] [-n NAME] [--host HOST]";
+    echo "Usage: submit python_file [-h] [-g NB_GPUS] [-c NB_CORES] [-d ROOT_DIR]";
+    echo "$indentation[-w WALLTIME] [-n NAME] [--host HOST]";
 
-	echo "";
-	echo "Submit a job on Jean Zay.";
+    echo "";
+    echo "Submit a job on Jean Zay.";
 
-	echo "";
+    echo "";
 
-	echo "optional arguments:";
-	small_indentation="         ";
-	echo "$small_indentation -h, --help$small_indentation  show this help message and exit";
-	echo "$small_indentation -g, --gpus$small_indentation  set the required number of GPUs (default: ${nb_gpus})";
-	echo "$small_indentation -c, --cores$small_indentation set the required number of cores (default: ${nb_cores})";
-	# echo "$small_indentation -n, --nodes$small_indentation set the required number of nodes";
-	echo "$small_indentation --dir$small_indentation       SLURM scripts dir (default: ${script_dir}";
-	echo "$small_indentation -o --out$small_indentation    SLURM logs dir (default: ${out}";
-	echo "$small_indentation -w, --wt$small_indentation    set the job walltime (default: ${walltime})";
-	echo "$small_indentation -n, --name$small_indentation  set the experiment name (default: \$python_file)";
-    echo "$small_indentation --resubmit$small_indentation  number of time the job should be resubmitted (default: 0)";
-	echo "$small_indentation --dev$small_indentation       run on the dev partition";
-	echo "$small_indentation --[data science 2.0 options]";
-	exit 1
+    echo "optional arguments:";
+    small_indentation="         ";
+    echo "$small_indentation -h, --help$small_indentation  show this help message and exit";
+    echo "$small_indentation -g, --gpu$small_indentation  set the required number of GPUs (default: ${nb_gpus})";
+    echo "$small_indentation -c, --cores$small_indentation set the required number of cores (default: ${nb_cores})";
+    # echo "$small_indentation -n, --nodes$small_indentation set the required number of nodes";
+    echo "$small_indentation --dir$small_indentation       SLURM scripts dir (default: ${script_dir}";
+    echo "$small_indentation -o --out$small_indentation    SLURM logs dir (default: ${out}";
+    echo -n "$small_indentation -w, --wt$small_indentation    set the job walltime (default: ${walltime})";
+    echo ", hours if int, can be >20";
+    echo "$small_indentation -n, --name$small_indentation  set the experiment name (default: \$python_file)";
+    echo "$small_indentation --dev$small_indentation       run on the dev partition";
+    echo "$small_indentation --[data science 2.0 options]";
+    exit 1
 }
 
 execute() {
     name=NULL;
 
     python_file=NULL;
-    reSubmission=0
 
     dev=false;
 
     project_path="~/Data-science-2.0";
 
     options="";
-
     while [[ "$1" != "" ]]; do
         case $1 in
-            --resubmit )            shift
-                                    reSubmission=$1;
-                                    ;;
             -g | --gpu )            shift
                                     nb_gpus=$1;
                                     ;;
@@ -77,7 +71,6 @@ execute() {
                                     else
                                         options="$options $1";
                                     fi
-
         esac
         shift
     done
@@ -114,92 +107,84 @@ execute() {
     then
         options="$options --gpu $gpu_command";
     fi
-
-    # constructing script
-    echo "CREATING SCRIPT $script_dir/${setup_name}$name.slurm"
-
-    echo "#!/bin/bash" > ${script_dir}/${setup_name}${name}.slurm
-    echo "#SBATCH --job-name=${setup_name}$name         # nom du job" >> ${script_dir}/${setup_name}${name}.slurm
-    if [[ ${dev} == false ]];
-    then
-        echo "#SBATCH --partition=gpu_gct3">> ${script_dir}/${setup_name}${name}.slurm
-    else
-        echo "#SBATCH --partition=gpu_dev">> ${script_dir}/${setup_name}${name}.slurm
-    fi
-
-    echo "#SBATCH  --mem=160G">> ${script_dir}/${setup_name}${name}.slurm
-    echo "#SBATCH  --cpus-per-task=4">> ${script_dir}/${setup_name}${name}.slurm
-    echo "#SBATCH --gres=gpu:$nb_gpus  # nombre de GPU à réserver">> ${script_dir}/${setup_name}${name}.slurm
-    echo "#SBATCH --time=$walltime             # (HH:MM:SS)">> ${script_dir}/${setup_name}${name}.slurm
-    echo "#SBATCH --output=${out}/${setup_name}${name}_%j.out" >> ${script_dir}/${setup_name}${name}.slurm
-    echo "#SBATCH --error=${out}/${setup_name}${name}_%j.err" >> ${script_dir}/${setup_name}${name}.slurm
-
-    echo >> ${script_dir}/${setup_name}${name}.slurm
-
-    echo "module load pytorch-gpu/py3/1.1" >> ${script_dir}/${setup_name}${name}.slurm
-    echo "module load p7zip/16.02/gcc-9.1.0" >> ${script_dir}/${setup_name}${name}.slurm
-
-
-    echo >> ${script_dir}/${setup_name}${name}.slurm
-
-    echo "# echo des commandes lancées" >> ${script_dir}/${setup_name}${name}.slurm
-    echo "set -x" >> ${script_dir}/${setup_name}${name}.slurm
-
-    echo "# exécution du code" >> ${script_dir}/${setup_name}${name}.slurm
-
+    # Jean Zay
     options="$options --homex /gpfswork/rech/fqg/uid61lx/output/";
-
-    exec_command="core=\"python $python_file$options\"";  # TODO add export folder
-    echo ${exec_command} >> ${script_dir}/${setup_name}${name}.slurm;
-    echo "export PYTHONPATH=\"${PYTHONPATH}:.\"" >> ${script_dir}/${setup_name}${name}.slurm;
-    echo "echo \"\$core\";" >> ${script_dir}/${setup_name}${name}.slurm
-    echo "\$core;" >> ${script_dir}/${setup_name}${name}.slurm
-
-    # first job
-    echo "sbatch ${script_dir}/${setup_name}${name}.slurm";
-    JOB_ID=`sbatch ${script_dir}/${setup_name}${name}.slurm | cut -d " " -f 4`
-
-    # resubmit successive jobs
-    if [[ ${reSubmission} -gt 0 ]];
-    then
-        for i in {1..${reSubmission}};
+    # with or without resubmission
+    re='^[0-9]+$'
+    JOB_ID=NULL;
+    if ! [[ ${walltime} =~ $re ]] ; then
+        actualWalltime=${walltime};
+        actualName="$name";
+        createScript;
+        echo ${JOB_ID};
+    else
+        i=1;
+        while [[ ${walltime} -gt 0 ]]
         do
-            echo "sbatch --dependency=afterany:${JOB_ID} ${script_dir}/${setup_name}${name}.slurm";
-            JOB_ID=`sbatch --dependency=afterany:${JOB_ID} ${script_dir}/${setup_name}${name}.slurm | cut -d " " -f 4`;
+            actualWalltime="$((walltime>20 ? 20 : walltime)):00:00";
+            actualName="${name}_part_$i";
+
+            createScript;
+            if [[ ${options} != *" -r"* ]] && [[ ${options} != *" --r"* ]];
+            then
+                options="$options --restart";
+            fi
+            walltime=$((walltime-20));
+            i=$((i+1))
         done;
     fi
-    # chargement des modules
 
-    # salloc --ntasks=1 --threads-per-core=1 --gres=gpu:1 --partition=gpu_dev --time=00:05:00
-    # module load ...
-    # srun --ntasks=1 --gres=gpu:1 python mon_script.py
-    # srun --ntasks=1 --gres=gpu:1 python -c "import torch; val=torch.cuda.is_available(); print('val = ', val);"
+}
+
+createScript() {
+    # constructing script
+    echo "CREATING SCRIPT $script_dir/${setup_name}$actualName.slurm"
+
+    echo "#!/bin/bash" > ${script_dir}/${setup_name}${actualName}.slurm
+    echo "#SBATCH --job-name=${setup_name}$actualName         # nom du job" >> ${script_dir}/${setup_name}${actualName}.slurm
+    if [[ ${dev} == false ]];
+    then
+        echo "#SBATCH --partition=gpu_gct3">> ${script_dir}/${setup_name}${actualName}.slurm
+    else
+        echo "#SBATCH --partition=gpu_dev">> ${script_dir}/${setup_name}${actualName}.slurm
+    fi
+
+    echo "#SBATCH  --mem=160G">> ${script_dir}/${setup_name}${actualName}.slurm
+    echo "#SBATCH  --cpus-per-task=4">> ${script_dir}/${setup_name}${actualName}.slurm
+    echo "#SBATCH --gres=gpu:$nb_gpus  # nombre de GPU à réserver">> ${script_dir}/${setup_name}${actualName}.slurm
+    echo "#SBATCH --time=$actualWalltime             # (HH:MM:SS)">> ${script_dir}/${setup_name}${actualName}.slurm
+    echo "#SBATCH --output=${out}/${setup_name}${actualName}_%j.out" >> ${script_dir}/${setup_name}${actualName}.slurm
+    echo "#SBATCH --error=${out}/${setup_name}${actualName}_%j.err" >> ${script_dir}/${setup_name}${actualName}.slurm
+
+    echo >> ${script_dir}/${setup_name}${actualName}.slurm
+
+    echo "module load pytorch-gpu/py3/1.1" >> ${script_dir}/${setup_name}${actualName}.slurm
+    echo "module load p7zip/16.02/gcc-9.1.0" >> ${script_dir}/${setup_name}${actualName}.slurm
 
 
+    echo >> ${script_dir}/${setup_name}${actualName}.slurm
 
-    # EXAMPLE PERL
-    # #!/usr/bin/perl
-    #my @tasks = split(',', $ARGV[0]);
-    #my @nodes = `scontrol show hostnames $SLURM_JOB_NODELIST`;
-    #my $node_cnt = $#nodes + 1;
-    #my $task_cnt = $#tasks + 1;
-    #
-    #if ($node_cnt < $task_cnt) {
-    #	print STDERR "ERROR: You only have $node_cnt nodes, but requested layout on $task_cnt nodes.\n";
-    #	$task_cnt = $node_cnt;
-    #}
-    #
-    #my $cnt = 0;
-    #my $layout;
-    #foreach my $task (@tasks) {
-    #	my $node = $nodes[$cnt];
-    #	last if !$node;
-    #	chomp($node);
-    #	for(my $i=0; $i < $task; $i++) {
-    #		$layout .= "," if $layout;
-    #		$layout .= "$node";
-    #	}
-    #	$cnt++;
-    #}
-    # print $layout;
+    echo "# echo des commandes lancées" >> ${script_dir}/${setup_name}${actualName}.slurm
+    echo "set -x" >> ${script_dir}/${setup_name}${actualName}.slurm
+
+    echo "# exécution du code" >> ${script_dir}/${setup_name}${actualName}.slurm
+
+    exec_command="core=\"python $python_file$options\"";  # TODO add export folder
+    echo ${exec_command} >> ${script_dir}/${setup_name}${actualName}.slurm;
+    echo "export PYTHONPATH=\"${PYTHONPATH}:.\"" >> ${script_dir}/${setup_name}${actualName}.slurm;
+    echo "echo \"\$core\";" >> ${script_dir}/${setup_name}${actualName}.slurm
+    echo "\$core;" >> ${script_dir}/${setup_name}${actualName}.slurm
+
+
+    # first job
+    if [[ "${JOB_ID}" = NULL ]];
+    then
+        echo "sbatch ${script_dir}/${setup_name}${actualName}.slurm";
+        JOB_ID=12;
+        JOB_ID=`sbatch ${script_dir}/${setup_name}${actualName}.slurm | cut -d " " -f 4`
+    else
+        echo "sbatch --dependency=afterany:${JOB_ID} ${script_dir}/${setup_name}${actualName}.slurm";
+        JOB_ID=$((JOB_ID+1));
+        JOB_ID=`sbatch --dependency=afterany:${JOB_ID} ${script_dir}/${setup_name}${actualName}.slurm | cut -d " " -f 4`;
+    fi
 }
