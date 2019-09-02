abe2467aae451a079c4f5e3216a1b2a601efb4ae
Servajean Maximilien <maximiliense@gmail.com>
2019-09-02 15:04:33 +0200

    save full checkpoint bug correction

--- projects/max_env/jean_zay.py
+++ projects/max_env/jean_zay.py
@@ -1,11 +1,12 @@
-from datascience.ml.neural.models import load_create_nn, InceptionEnv
+from datascience.ml.neural.models import InceptionEnv
 from datascience.data.loader import occurrence_loader
 from datascience.data.datasets import EnvironmentalIGNDataset
 from datascience.ml.neural.supervised import fit
+from datascience.ml.neural.supervised.train.checkpoints import create_model
 from projects.max_env.configs.inception import training_params, validation_params, model_params, optim_params
 
 # creating environmental inception (more channels than classical inception)
-model = load_create_nn(model_class=InceptionEnv, model_params=model_params)
+model = create_model(model_class=InceptionEnv, model_params=model_params)
 
 # loading dataset
 train, val, test = occurrence_loader(EnvironmentalIGNDataset, source='full_ign_5m')
