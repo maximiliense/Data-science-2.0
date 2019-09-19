b082150e65c3ea785a81a1b01013597a6dc74877
zhelloz <benjamindeneu@free.fr>
2019-09-19 14:12:17 +0200

    update best of labs to re-run on 2019 test set

--- projects/best_of_labs/inception_env.py
+++ projects/best_of_labs/inception_env.py
@@ -1,23 +1,26 @@
-from datascience.ml.metrics import ValidationAccuracyMultipleBySpecies, ValidationMRRBySpecies
+from datascience.ml.metrics import ValidationAccuracyMultipleBySpecies, ValidationMRRBySpecies, JustExportPredictions
 from datascience.ml.metrics import ValidationAccuracyRangeBySpecies, ValidationAccuracyForAllSpecies
 from datascience.ml.neural.models import load_create_nn, InceptionEnv
 from datascience.data.loader import occurrence_loader
 from datascience.data.datasets import EnvironmentalDataset
 from datascience.ml.neural.supervised import fit
-from datascience.ml.neural.supervised.predict import predict
-from datascience.model_selection import train_test_split_stratified
-from datascience.ml.neural.loss.loss import CELoss
+from datascience.ml.neural.supervised.predict.predict_grid import predict_grid
 from datascience.ml.evaluation.export import export_results
+from datascience.ml.evaluation.filters import FilterLabelsList
 
-from projects.ecography.configs.inception import model_params, training_params
+from projects.best_of_labs.configs.inception import model_params, training_params
 
 # loading/creating model
 model = load_create_nn(model_class=InceptionEnv, model_params=model_params)
 
 # loading dataset
-_, _, test = occurrence_loader(EnvironmentalDataset, source='glc19_test', splitter=train_test_split_stratified, test_size=1)
+_, _, test = occurrence_loader(EnvironmentalDataset, source='glc19_test',
+                               test_size=1, id_name='glc19TestOccId', label_name=None)
 
-print(type(test))
-predictions, labels, _ = predict(model, test, CELoss, export=True)
+predictions = predict_grid(model, test, batch_size=128, features_activation=False, logit=False)
+
+f = FilterLabelsList('/home/benjamin/pycharm/Data-science-2.0/projects/best_of_labs/allowed_classes.txt')
+
+f(predictions)
 
 export_results(test, predictions)
