0021784efa501fe60791001452d33d7d7b940b48
Servajean Maximilien <maximiliense@gmail.com>
2019-11-04 12:53:05 +0100

    deep analysis + glc19 test

--- projects/stability_regularity_deep_learning/figure_1_2_3.py
+++ projects/stability_regularity_deep_learning/figure_1_2_3.py
@@ -3,15 +3,15 @@ from datascience.ml.neural.supervised import fit
 from datascience.ml.neural.models import FullyConnectedDeepAnalysis
 from datascience.ml.neural.checkpoints import create_model
 
-from datascience.visu.deep_test_plots import plot_db_partitions_gradients
-from datascience.visu.util.util import save_fig
+from datascience.visu.deep_test_plots import plot_db_partitions_gradients, plot_separator
+from datascience.visu.util.util import save_fig, remove_axis
 
 # constructing the dataset
 train, test = create_dataset(param_train=(250, 250), poly=True)
 
 # creating/loading a model
 model_params = {
-    'architecture': (8,),  # play with config GD and SGD + architecture for the first figures
+    'architecture': (10, 10, 10),  # play with config GD and SGD + architecture for the first figures
     'dropout': 0.0,
 }
 model = create_model(model_class=FullyConnectedDeepAnalysis, model_params=model_params)
@@ -33,6 +33,10 @@ fit(
 )
 
 # plot results
-plot_db_partitions_gradients(train.dataset, train.labels, model)
+ax = plot_db_partitions_gradients(train.dataset, train.labels, model)
+
+plot_separator(train.separator, ax=ax)
+
+remove_axis()
 
 save_fig()
