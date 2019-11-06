7552b1374af6d0b42a0654e2dadba5e82abed9f9
Servajean Maximilien <maximiliense@gmail.com>
2019-11-06 16:03:19 +0100

    updated sources

--- projects/max_env/configs/test.py
+++ projects/max_env/configs/test.py
@@ -1,7 +1,16 @@
+from sync.datascience.engine.parameters.special_parameters import get_setup_path
+
+from datascience.ml.evaluation.filters import FilterLabelsList
+
 config = {
     'occurrence_loader': {
         'source': "glc19_test",
         'validation_size': 0.,
         'test_size': 1.
+    },
+    'fit': {
+        'predict_params': {
+                'filters': (FilterLabelsList(get_setup_path() + '/allowed_classes_final.txt'),)
+            }
     }
 }
