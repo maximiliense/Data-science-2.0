71e88a87033638ee2580c164a6a494a5aaef4e85
Servajean Maximilien <maximiliense@gmail.com>
2019-12-22 17:02:21 +0100

    painter prediction

---
+++ datascience/data/datasets/image_dataset.py
@@ -0,0 +1,24 @@
+from torch.utils.data import Dataset
+import torch
+import numpy as np
+from PIL import Image
+
+
+class ImageDataset(Dataset):
+    def __init__(self, dataset, transform=None):
+        """
+        :param dataset:
+        """
+        self.dataset = dataset
+        self.transform = transform
+
+    def __len__(self):
+        return len(self.dataset)
+
+    def __getitem__(self, idx):
+        img = Image.open(self.dataset[idx][0])
+
+        if self.transform is not None:
+            img = self.transform(img)
+
+        return img, self.dataset[idx][1]
