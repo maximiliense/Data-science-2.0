d87377801aca402ec75ac2842d82f35f496217b2
Servajean Maximilien <maximiliense@gmail.com>
2020-01-15 16:40:13 +0100

    loss

--- datascience/ml/neural/loss/loss.py
+++ datascience/ml/neural/loss/loss.py
@@ -54,13 +54,10 @@ class MTBCEWithLogitsLoss(Loss):
         self.criterion = nn.BCEWithLogitsLoss(*args, **kwargs)
 
     def loss(self, output, label):
-        print('VALENTIN!!')
-        print(output.size(), label.size())
-        exit()
-        _output = torch.mul(output, label[0])
+        _output = torch.mul(output, label[:, 0])
         # bias because the criterion should not take into account the masked values
-        bias = torch.log(torch.sigmoid(_output[label[0] == 0])).sum()/(output.size(0)*_output.size(1))
-        return self.criterion(_output, label[1]) + bias
+        bias = torch.log(torch.sigmoid(_output[label[:, 0] == 0])).sum()/(output.size(0)*_output.size(1))
+        return self.criterion(_output, label[:, 1]) + bias
 
     def __repr__(self):
         return 'Multi-task Binary Cross Entropy'
